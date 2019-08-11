175983de837289b5767468c99032c6c681e09ce5
wenhui <wenhuigl@gmail.com>
2019-08-11 18:16:56 +0800

    vue组件传值

---
+++ todolist/src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <li @click="dodelete">{{content}}</li>
+</template>
+
+<script>
+    export default {
+        name: "TodoItem",
+        props: ['content', 'index'],
+        methods: {
+            dodelete () {
+                this.$emit('mydelete', this.index)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ todolist/src/views/About.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <input v-model="inputValue">
+    <button @click="submit">提交</button>
+    <ul>
+      <todo-item v-for="(item, index) in list" :content="item" :index="index" @mydelete="handledelete"></todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import TodoItem from '../components/TodoItem'
+
+  export default {
+    components: {
+      'todo-item': TodoItem
+    },
+    data () {
+      return {
+        inputValue: '',
+        list: []
+      }
+    },
+    methods: {
+      submit () {
+        this.list.push(this.inputValue)
+        this.inputValue = ''
+      },
+      handledelete (index) {
+        this.list.splice(index, 1)
+      }
+    }
+  }
+</script>
